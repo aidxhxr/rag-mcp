7e9de6af3bf2d972857cd5f476baa7451c8227d3
CyberOmerkhan <amazingOmerkhan@gmail.com>
2026-05-30 14:54:22 -0400

    UPDATE: clerk setup & agentic skills added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import {ClerkProvider} from "@clerk/nextjs";
 import type { Metadata } from "next";
 import {
   Geist,
@@ -60,8 +61,10 @@ export default function RootLayout({
       )}
     >
       <body className="min-h-full flex flex-col">
-        <Navbar />
-        {children}
+        <ClerkProvider>
+          <Navbar />
+          {children}
+        </ClerkProvider>
       </body>
     </html>
   );

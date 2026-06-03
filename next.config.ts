3df7f823f50d490c3aecbcf1c54c74b5414490c4
CyberOmerkhan <amazingOmerkhan@gmail.com>
2026-06-02 20:17:17 -0400

    UPDATE: starting from scratch due to MongoDB incompatibility

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [new URL("https://covers.openlibrary.org/**")],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
